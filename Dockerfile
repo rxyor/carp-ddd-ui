0d8cda72ceab1af096c4555afdff1691881fd4fd
错误 <liuyangrxy@163.com>
2020-02-22 23:33:59 +0800

    Jenkins构建配置相关脚本

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,15 @@
-FROM nginx:latest
+FROM node
+WORKDIR /app
+COPY package*.json ./
+RUN npm install -g cnpm --registry=https://registry.npm.taobao.org
+RUN cnpm install
+COPY ./ /app
+RUN npm run build
+
+FROM nginx
 MAINTAINER ly <liuyangrxy@163.com>
-COPY dist/ /usr/share/nginx/html
-VOLUME /tmp
-EXPOSE 80
+RUN mkdir /app
+COPY --from=0 /app/dist /app
+COPY nginx.conf /etc/nginx/nginx.conf
+
 
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+image_version=`date +%Y%m%d%H%M`;
+# 关闭carp-ui容器
+docker stop carp-ui || true;
+# 删除carp-ui容器
+docker rm carp-ui || true;
+# 删除carp-ui镜像
+docker rmi --force $(docker images | grep carp-ui | awk '{print $3}')
+# 构建carp-ui:$image_version镜像
+docker build . -t carp-ui:$image_version;
+# 查看镜像列表
+docker images;
+# 基于carp-ui 镜像 构建一个容器 carp-ui
+docker run -p 9527:80 -d --name carp-ui carp-ui:$image_version;
+# 查看日志
+docker logs carp-ui;
+#删除build过程中产生的镜像    #docker image prune -a -f
+docker rmi $(docker images -f "dangling=true" -q)
+# 对空间进行自动清理
+docker system prune -a -f
+
