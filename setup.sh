4fe9d26a9760b0480ce746b9f7368c9fbc688aab
错误 <liuyangrxy@163.com>
2020-02-23 09:22:14 +0800

    修改配置

--- setup.sh
+++ setup.sh
@@ -11,11 +11,10 @@ docker build . -t carp-ui:$image_version;
 # 查看镜像列表
 docker images;
 # 基于carp-ui 镜像 构建一个容器 carp-ui
-docker run -p 9527:80 -d --name carp-ui carp-ui:$image_version;
+docker run -p 9527:80 -d --name carp-ui --add-host carp-gateway:192.168.0.116 carp-ui:$image_version ;
 # 查看日志
 docker logs carp-ui;
 #删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images -f "dangling=true" -q)
+# docker rmi $(docker images -f "dangling=true" -q)
 # 对空间进行自动清理
-docker system prune -a -f
-
+# docker system prune -a -f
